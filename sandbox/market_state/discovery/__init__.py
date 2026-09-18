01a8a4e96255819fe57a83e21035dd00e64e9f25
Surya Sudhakar <Surya.Sudhakar@stud.srh-university.de>
2026-09-18 13:40:53 +0200

    gmm: export discovery scaler contract

--- sandbox/market_state/discovery/__init__.py
+++ sandbox/market_state/discovery/__init__.py
@@ -1,5 +1,9 @@
 """Unsupervised market-state discovery models."""
 
 from .model import StateDiscoveryModel, validate_state_matrix
+from .scaler import DiscoveryStandardScaler, DiscoveryScalerMetadata
 
-__all__ = ["StateDiscoveryModel", "validate_state_matrix"]
+__all__ = [
+    "StateDiscoveryModel", "validate_state_matrix",
+    "DiscoveryStandardScaler", "DiscoveryScalerMetadata",
+]
